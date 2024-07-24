79fb2c58bf022a4b764bdf7b4b024ee6399fb7fe
lcohen730 <lcohen730@gmail.com>
2024-07-24 17:17:19 -0400

    Add Pages Files and Plan Out Possible Components

---
+++ src/pages/App/App.js
@@ -0,0 +1,16 @@
+// Components Needed
+
+// Header
+    // Logo
+    // ModeSwitch
+// SearchBar
+// FilterDropDown
+// FilterOptions
+// CountryList
+// CountryDetail
+// CountryDetailInfo
+// BackButton
+// BorderCountryOptions
+// CountryListItem
+// CountryListDetail
+// Flag
