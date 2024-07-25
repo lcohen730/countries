76146d3a2eeffe5928d2c4b682e2329beb28fbf1
lcohen730 <lcohen730@gmail.com>
2024-07-25 18:50:43 -0400

    Add Functions and Update Routes to App Page

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,14 +1,49 @@
+import React, { useState } from 'react';
+import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import DetailPage from '../DetailPage/DetailPage';
 import HomePage from '../HomePage/HomePage';
 import Header from '../../components/Header/Header';
 
 export default function App() {
+    const [darkMode, setDarkMode] = useState(false);
+    const navigate = useNavigate();
+
+    function handleModeSwitch() {
+        setDarkMode(!darkMode);
+    }
+
+    async function handleSelectCountry(countryCode) {
+        navigate(`/detail/${countryCode}`);
+    }
+
+    function handleSearchCountry(searchTerm) {
+        navigate(`/search/${searchTerm}`);
+    }
+
+    function handleFilterCountry(region) {
+        navigate(`/filter/${region}`);
+    }
+
     return (
         <main>
-            <Header />
+            <Header handleModeSwitch={handleModeSwitch} />
             <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/detail/:id" element={<DetailPage />} />
+                <Route path="/" 
+                    element={<HomePage 
+                        handleSelectCountry={handleSelectCountry} 
+                        handleSearchCountry={handleSearchCountry} 
+                        handleFilterCountry={handleFilterCountry} 
+                    />} 
+                />
+                <Route path="/search/:searchTerm" 
+                    element={<HomePage 
+                        handleSelectCountry={handleSelectCountry} 
+                        handleSearchCountry={handleSearchCountry} 
+                        handleFilterCountry={handleFilterCountry} 
+                    />} 
+                />
+                <Route path="/detail/:countryCode" element={<DetailPage />} />
+                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </main>
     );
