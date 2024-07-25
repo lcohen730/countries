995602c3daf09de2885847b07ed8274879234c29
lcohen730 <lcohen730@gmail.com>
2024-07-25 02:41:28 -0400

    Add Starter Code for App Page

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,16 +1,15 @@
-// Components Needed
+import DetailPage from '../DetailPage/DetailPage';
+import HomePage from '../HomePage/HomePage';
+import Header from '../../components/Header/Header';
 
-// Header
-    // Logo
-    // ModeSwitch
-// SearchBar
-// FilterDropDown
-// FilterOptions
-// CountryList
-    // CountryListItem
-        // CountryListDetail
-// CountryDetail
-    // CountryDetailInfo
-    // BorderCountryOptions
-// BackButton
-// Flag
+export default function App() {
+    return (
+        <main>
+            <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/detail/:id" element={<DetailPage />} />
+            </Routes>
+        </main>
+    );
+}
